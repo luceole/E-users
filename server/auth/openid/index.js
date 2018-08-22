89cd1b00eb3de1e9b59145dfa5f53ee5ffb2acb5
luc <luc.bourdot@ac-dijon.fr>
2018-08-22 18:03:19 +0200

    Fix Open ID: Nonce is needed for identity provider like github

--- server/auth/openid/index.js
+++ server/auth/openid/index.js
@@ -2,43 +2,53 @@
 
 import express from 'express';
 import passport from 'passport';
-import {setTokenCookie} from '../auth.service';
+import {
+  setTokenCookie
+} from '../auth.service';
 import config from '../../config/environment';
 
 var router = express.Router();
 
 router
-  .get('/', passport.authenticate('openid', {session: false}))
+  .get('/', passport.authenticate('openid', {
+    session: false
+  }))
+  // .get('/callback', passport.authenticate('openid', {
+  //   successRedirect: '/',
+  //   failureRedirect: '/login'
+  // }))
+
+
   .get('/callback', function(req, res, next) {
     passport.authenticate('openid', {
       session: false
     }, function(err, user) {
-      if(err) return next(err);
+      if (err) return next(err);
       // Redirect if it fails
-      if(!user) return res.redirect('/signup');
+      if (!user) return res.redirect('/signup');
       req.user = user;
       req.session.openid = user.openid;
       return next();
     })(req, res, next);
   }, setTokenCookie)
-/*
-  .get('/logout', function(req, res) {
-  const session = req.session;
-  const tokenSet = session && session.openid && session.openid.tokenSet ? session.openid.tokenSet : null;
-  if(!tokenSet) return res.status(200).end();
-  const redirectUrl = req.get('referer');
-  const params = `post_logout_redirect_uri=${redirectUrl}&id_token_hint=${tokenSet.id_token}`;
-  const endSessionEndpoint = session.openid.issuer.end_session_endpoint;
-  return res.redirect(`${endSessionEndpoint}?${params}`);
- })
-*/
-.get('/logout', function(req, res, next) {
-  const redirectUrl = req.get('referer');
-  const params = `post_logout_redirect_uri=${redirectUrl}}`;
-  req.logout();
-  var urlLogout = `${config.openid.discover}logout`;
-  res.redirect(`${urlLogout}?${params}`);
-  return next();
-});
+  /*
+    .get('/logout', function(req, res) {
+    const session = req.session;
+    const tokenSet = session && session.openid && session.openid.tokenSet ? session.openid.tokenSet : null;
+    if(!tokenSet) return res.status(200).end();
+    const redirectUrl = req.get('referer');
+    const params = `post_logout_redirect_uri=${redirectUrl}&id_token_hint=${tokenSet.id_token}`;
+    const endSessionEndpoint = session.openid.issuer.end_session_endpoint;
+    return res.redirect(`${endSessionEndpoint}?${params}`);
+   })
+  */
+  .get('/logout', function(req, res, next) {
+    const redirectUrl = req.get('referer');
+    const params = `post_logout_redirect_uri=${redirectUrl}}`;
+    req.logout();
+    var urlLogout = `${config.openid.discover}logout`;
+    res.redirect(`${urlLogout}?${params}`);
+    return next();
+  });
 
 export default router;
--- server/config/local.env.sample.js
+++ server/config/local.env.sample.js
@@ -30,20 +30,55 @@ module.exports = {
     site: 'http://localhost:3000',
     url: 'http://localhost:3000/api/users/validate/'
   },
-  onlineServices: [
-    {glyphicon: 'glyphicon-bullhorn', url: 'https://forum.domain.fr', title: ' Forum Libre Communauté', target: '_self'},
-    {glyphicon: 'glyphicon-certificate', url: 'https://chat.domain.fr', title: 'Chat Libre Communauté'  target: '_blank'}
+  onlineServices: [{
+      glyphicon: 'glyphicon-bullhorn',
+      url: 'https://forum.domain.fr',
+      title: ' Forum Libre Communauté',
+      target: '_self'
+    },
+    {
+      glyphicon: 'glyphicon-certificate',
+      url: 'https://chat.domain.fr',
+      title: 'Chat Libre Communauté'
+      target: '_blank'
+    }
   ],
-  Structures: [
-      {id: 'Education', value: 'Education'},
-      {id: 'Ecologie', value: 'Ecologie'},
-      {id: 'Affaires Etrangères', value: 'Affaires Etrangères'},
-      {id: 'Intérieur', value: 'Intérieur'},
-      {id: 'Affaires Sociales', value: 'Affaires Sociales'},
-      {id: 'Finance', value: 'Finance'},
-      {id: 'Justice', value: 'Justice'},
-      {id: 'Collectivité', value: 'Collectivité'},
-      {id: 'Autre', value: 'Autre'}
+  Structures: [{
+      id: 'Education',
+      value: 'Education'
+    },
+    {
+      id: 'Ecologie',
+      value: 'Ecologie'
+    },
+    {
+      id: 'Affaires Etrangères',
+      value: 'Affaires Etrangères'
+    },
+    {
+      id: 'Intérieur',
+      value: 'Intérieur'
+    },
+    {
+      id: 'Affaires Sociales',
+      value: 'Affaires Sociales'
+    },
+    {
+      id: 'Finance',
+      value: 'Finance'
+    },
+    {
+      id: 'Justice',
+      value: 'Justice'
+    },
+    {
+      id: 'Collectivité',
+      value: 'Collectivité'
+    },
+    {
+      id: 'Autre',
+      value: 'Autre'
+    }
   ],
   discourse_sso: {
     secret: '1234567890',
@@ -54,12 +89,13 @@ module.exports = {
     discover: 'https://server.mondomaine.fr/.well-known/openid-configuration',
     issuer: {
       issuer: 'https://server.mondomaine.fr/auth/realms/sso/',
-    //   authorization_endpoint: `${process.env.OPENID_ISSUER}/auth`,
-    //   token_endpoint: `${process.env.OPENID_ISSUER}/token`,
-    //   userinfo_endpoint: `${process.env.OPENID_ISSUER}/userinfo`,
-    //   jwks_uri: `${process.env.OPENID_ISSUER}/certs`,
-    //   end_session_endpoint: `${process.env.OPENID_ISSUER}/logout`
+      //   authorization_endpoint: `${process.env.OPENID_ISSUER}/auth`,
+      //   token_endpoint: `${process.env.OPENID_ISSUER}/token`,
+      //   userinfo_endpoint: `${process.env.OPENID_ISSUER}/userinfo`,
+      //   jwks_uri: `${process.env.OPENID_ISSUER}/certs`,
+      //   end_session_endpoint: `${process.env.OPENID_ISSUER}/logout`
     },
+    nonce: "a basic random string",
     client: {
       client_id: 'e-users',
       client_secret: '...................................',
