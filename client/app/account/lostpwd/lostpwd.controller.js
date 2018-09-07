43914827c6755fdc650017a3a54d0a50be73a008
luc <luc.bourdot@ac-dijon.fr>
2018-09-07 15:22:02 +0200

    Fix CaseSensitive on email when ask for a new password

--- client/app/account/lostpwd/lostpwd.controller.js
+++ client/app/account/lostpwd/lostpwd.controller.js
@@ -23,8 +23,8 @@ export default class LostpwdController {
 
   register(form) {
     this.submitted = true;
-    if(form.$valid) {
-      this.Auth.lostPassword(this.email)
+    if (form.$valid) {
+      this.Auth.lostPassword(this.email.toLOwerCase())
         .then(() => {
           this.message = `Initialisation du mot de passe: courriel envoyé à ${this.email}`;
         })
